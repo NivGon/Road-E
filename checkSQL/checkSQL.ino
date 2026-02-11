26e2cd902174cd225a66040d5dd4a44480aa7c08
arielgal56 <arielgal56@gmail.com>
2026-02-11 16:24:36 +0200

    Update Code

--- checkSQL/checkSQL.ino
+++ checkSQL/checkSQL.ino
@@ -11,11 +11,14 @@
   1. add library ArduinoJson - to handle data format in "clean" way 
   2. add settings for WIFI connection
   3. add declare for server URL - IP of the computer
+  4. run once, don't working.
   
 */
 
 //libraries
 #include <Wire.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include <Adafruit_AHTX0.h>
 #include <ESP32Servo.h>
 #include <ArduinoJson.h>
@@ -34,15 +37,15 @@ Adafruit_AHTX0 aht;
 #define LDR 34
 
 //Network Settings
-const char* ssid = "Modin-Students"; //WIFI Name 
-const char* password = ""; //WIFI Password
+const char* ssid = "Modin-Students";  //WIFI Name
+const char* password = "";            //WIFI Password
 
 //Server URL
-const char* serverName = "http://COMPUTER_IP:5000/data"; //need to update any time before running + need to keep :5000/data at the end.
+const char* serverName = "http://192.168.22.179:5000/upload";  //need to update any time before running + need to keep :5000/data at the end.
 
 
-void setup(){
-  Serial.begin(9600);
+void setup() {
+  Serial.begin(115200);
 
   //hcsr
   pinMode(Echo_Pin, INPUT);
@@ -57,77 +60,71 @@ void setup(){
   //LDR
   pinMode(LDR, INPUT);
 
-  //WIFI Check Connection
+  //WiFi Connection
   WiFi.begin(ssid, password);
-  Serial.println("Connecting to WIFI");
-  while(WiFi.status() != WL_CONNECTED){
-    dealy(500);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.print("Connected to WIFI network with IP Address: ");
-  Serial.println(Wifi.LocalIP());
+  Serial.println("Connected!");
 }
 
 
-void loop(){
-  sensors_event_t humidity, temp;
-  aht.getEvent(&humidity, &temp);
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
 
-  delay(100);
+    sensors_event_t humidity, temp;
+    aht.getEvent(&humidity, &temp);
+    float t = temp.temperature;
+    float h = humidity.relative_humidity;
+    delay(100);
 
-  int light = analogRead(LDR);
+    int light = analogRead(LDR);
+    delay(100);
 
-  dealy(100);
+    //calc distance hcsr04
+    long duration;
+    float distance;
+    digitalWrite(Trig_Pin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(Trig_Pin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(Trig_Pin, LOW);
+    duration = pulseIn(Echo_Pin, HIGH);
+    distance = duration * 0.034 / 2;
+    Serial.print("distance -> ");
+    Serial.println(distance);
 
-  //calc distance hcsr04
-  long duration;
-  float distance;
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-  duration = pulseIn(ECHO_PIN, HIGH);
-  distance = duration * 0.034 / 2;
-  Serial.print("distance -> ");
-  Serial.println(distance);
+    // --- DEBUG PRINT ---
+    Serial.printf("Temp: %.2f, Hum: %.2f, Dist: %.2f, LDR: %d\n", t, h, distance, light);
 
-  //Check WIFI Connection Status
-  if(WiFi.status() == WL_CONNECTED){
+    // --- SEND TO PYTHON ---
     HTTPClient http;
-
-    //Start Connection
     http.begin(serverName);
-    http.addHeader("Contect-Type", "application/json");
+    http.addHeader("Content-Type", "application/json");
+
+    // Construct JSON String
+    String jsonPayload = "{";
+    jsonPayload += "\"temp\":" + String(t) + ",";
+    jsonPayload += "\"hum\":" + String(h) + ",";
+    jsonPayload += "\"dist\":" + String(distance) + ",";
+    jsonPayload += "\"ldr\":" + String(light);
+    jsonPayload += "}";
+
+    int httpResponseCode = http.POST(jsonPayload);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println("Server Response: " + response);
+    } else {
+      Serial.print("Error sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+
+  } else {
+    Serial.println("WiFi Disconnected");
   }
 
-
-
-  
+  delay(5000);  // Send data every 5 seconds
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
